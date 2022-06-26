d52c63cf2a26ea2e768bbe291212fae40d554824
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2022-06-26 20:10:40 -0300

    Paralelizando tarefas em um serviço

--- src/main/java/ecommerce/LogService.java
+++ src/main/java/ecommerce/LogService.java
@@ -9,28 +9,23 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.regex.Pattern;
 
-public class FraudDetectorService {
+public class LogService {
   public static void main(String[] args) {
     try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties())) {
-      consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
+      consumer.subscribe(Pattern.compile("ECOMMERCE.*"));
       while (true) {
         ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
         if (!records.isEmpty()) {
           System.out.println("Find " + records.count() + " registers");
           for (ConsumerRecord<String, String> record : records) {
             System.out.println("----------------------------------");
-            System.out.println("Processing new order, checking for fraud");
+            System.out.println("LOG " + record.topic());
             System.out.println(record.key());
             System.out.println(record.value());
             System.out.println(record.partition());
             System.out.println(record.offset());
-            try {
-              Thread.sleep(5000);
-            } catch (InterruptedException e) {
-              e.printStackTrace();
-            }
-            System.out.println("Order processed");
           }
         }
       }
@@ -42,8 +37,7 @@ public class FraudDetectorService {
     properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
     properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
     properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-    properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
-    properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
+    properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, LogService.class.getSimpleName());
     return properties;
   }
 }
--- src/main/java/ecommerce/NewOrderMain.java
+++ src/main/java/ecommerce/NewOrderMain.java
@@ -1,26 +1,38 @@
 package ecommerce;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
   public static void main(String[] args) throws ExecutionException, InterruptedException {
     try (KafkaProducer<String, String> producer = new KafkaProducer<>(properties())) {
-      String value = "123,1212,545000";
-      ProducerRecord<String, String> record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-      producer.send(record, (data, ex) -> {
-        if (ex != null) {
-          ex.printStackTrace();
-          return;
-        }
-        System.out.println("Sucesso enviando " + data.topic() + ":::partition" + data.partition() + "/ offset" + data.offset() + "/ timestamp" + data.timestamp());
-      }).get();
+      for (int i = 0; i <= 100; i++) {
+        String value = "123,1212,545000";
+        String key = UUID.randomUUID().toString() + "123,1212,545000";
+        ProducerRecord<String, String> record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
+        producer.send(record, callback()).get();
+        String email = "Thank you for your order! We are processing your order!";
+        ProducerRecord<String, String> emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
+        producer.send(emailRecord, callback()).get();
+      }
     }
+  }
+
+  private static Callback callback() {
+    return (data, ex) -> {
+      if (ex != null) {
+        ex.printStackTrace();
+        return;
+      }
+      System.out.println("Sucesso enviando " + data.topic() + ":::partition" + data.partition() + "/ offset" + data.offset() + "/ timestamp" + data.timestamp());
+    };
   }
 
   private static Properties properties() {
